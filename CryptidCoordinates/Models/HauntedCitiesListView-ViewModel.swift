739f623fc128c0a711976dd9d6a85644a4c66a10
noahgiboney <noahgiboney@gmail.com>
2024-01-13 22:13:52 -0800

    fix: random typo and invalid location

---
+++ CryptidCoordinates/Models/HauntedCitiesListView-ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HauntedCitiesListView-ViewModel.swift
+//  CryptidCoordinates
+//
+//  Created by Noah Giboney on 1/13/24.
+//
+
+import Foundation
---
+++ CryptidCoordinates/ViewModifiers.swift
@@ -0,0 +1,10 @@
+//
+//  ViewModifiers.swift
+//  CryptidCoordinates
+//
+//  Created by Noah Giboney on 1/13/24.
+//
+
+import Foundation
+
+struct f
