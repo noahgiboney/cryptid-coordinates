1af62f4694cf35a4e87d801cf614343c424b46a4
noahgiboney <noahgiboney@gmail.com>
2024-01-18 20:34:41 -0800

    feat: header for selected location

---
+++ CryptidCoordinates/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  CryptidCoordinates
+//
+//  Created by Noah Giboney on 1/18/24.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
