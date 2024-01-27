4a8086ad1fd2313aad1e5fe9be036c679a78ec65
noahgiboney <noahgiboney@gmail.com>
2024-01-26 21:55:09 -0800

    chore: custom modifier for nav button

--- CryptidCoordinates/BundleDecode.swift
+++ CryptidCoordinates/BundleDecode.swift
@@ -2,31 +2,7 @@
 //  BundleDecode.swift
 //  CryptidCoordinates
 //
-//  Created by Noah Giboney on 1/9/24.
+//  Created by Noah Giboney on 1/26/24.
 //
 
 import Foundation
-
-extension Bundle {
-    func decode(file: String) -> [HauntedLocation] {
-        
-        //ensure json file is in the bundle
-        guard let url = self.url(forResource: file, withExtension: nil) else{
-            fatalError("Cannot find \(file) in app bundle")
-        }
-        
-        //load the data
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Cannot load data from \(file)")
-        }
-        
-        //decode data from json and return arr
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        guard let decodedLocations = try? decoder.decode([HauntedLocation].self, from: data) else{
-            fatalError("Unable to decode json from \(file)")
-        }
-        return decodedLocations
-    }
-}
-
