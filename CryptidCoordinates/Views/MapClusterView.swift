fd1e43c894714f5c78896a4cdd00f18a2dab11d3
noahgiboney <noahgiboney@gmail.com>
2024-02-03 16:15:37 -0800

    update: map annoations

--- CryptidCoordinates/Views/MapClusterView.swift
+++ CryptidCoordinates/Views/MapClusterView.swift
@@ -10,24 +10,10 @@ import SwiftUI
 struct MapClusterView: View {
     var body: some View {
         VStack {
-            ZStack {
-                LinearGradient(colors: [.black, .gray], startPoint: .bottom, endPoint: .top)
-                    .clipShape(Circle())
-                    .shadow(radius: 2, x:0, y: 2)
-                
-                Image(systemName: "square.3.layers.3d")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(.white)
-                    .padding(10)
-            }
-            .frame(width: 40, height: 40)
-            
-            Image(systemName: "triangle.fill")
-                .font(.system(size: 12))
-                .offset(y: 5)
-                .rotationEffect(.degrees(180))
-                .foregroundColor(.black)
+            Image(systemName: "square.3.layers.3d")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 25, height: 25)
         }
     }
 }
--- MapAnnotationView.swift
+++ MapAnnotationView.swift
@@ -10,24 +10,17 @@ import SwiftUI
 struct MapAnnotationView: View {
     var body: some View {
         VStack {
-            ZStack {
-                LinearGradient(colors: [.black,.red], startPoint: .bottom, endPoint: .top)
-                    .clipShape(Circle())
-                    .shadow(radius: 2, x:0, y: 2)
-    
-                Image("ghost")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(.white)
-                    .padding(5)
-            }
-            .frame(width: 40, height: 40)
+            Image("ghost")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.white)
+                .frame(width: 40, height: 40)
+                
             
-            Image(systemName: "triangle.fill")
-                .font(.system(size: 12))
-                .offset(y: 5)
-                .rotationEffect(.degrees(180))
-                .foregroundColor(.black)
+            Image(systemName: "mappin")
+                .font(.system(size: 15))
+                .foregroundColor(.white)
+                .padding(.bottom, 20)
         }
     }
 }
