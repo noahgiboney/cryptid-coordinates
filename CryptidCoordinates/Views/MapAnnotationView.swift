2130192c7f97cb8a88549e9193260459a9e4d518
noahgiboney <noahgiboney@gmail.com>
2024-02-08 17:35:36 -0800

    update: ghost logo

--- CryptidCoordinates/Views/MapAnnotationView.swift
+++ CryptidCoordinates/Views/MapAnnotationView.swift
@@ -11,13 +11,15 @@ struct MapAnnotationView: View {
     
     var body: some View {
         
-        Image("ghost")
+        Image("ghoul")
             .resizable()
             .scaledToFill()
             .frame(width: 40, height: 40)
+            .padding([.bottom, .leading], 25)
     }
 }
 
 #Preview {
     MapAnnotationView()
+        .preferredColorScheme(.dark)
 }
