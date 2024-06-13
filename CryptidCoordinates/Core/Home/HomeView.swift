da347cb6b4f63ff26832b21fb7550fbdf1f81bbe
Noah Giboney <noahgiboney@gmail.com>
2024-06-12 23:37:57 -0700

    setup view

---
+++ CryptidCoordinates/Core/Home/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  CryptidCoordinates
+//
+//  Created by Noah Giboney on 6/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                
+            }
+            .navigationTitle("Cryptid Coordinates")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
