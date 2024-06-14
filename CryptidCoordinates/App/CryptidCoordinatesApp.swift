0e16d71b8955bbd50b88e8fae98ab155d9266e9e
Noah Giboney <noahgiboney@gmail.com>
2024-06-13 19:29:18 -0700

    setup firebase

--- CryptidCoordinates/App/CryptidCoordinatesApp.swift
+++ CryptidCoordinates/App/CryptidCoordinatesApp.swift
@@ -5,16 +5,24 @@
 //  Created by Noah Giboney on 1/9/24.
 //
 
-import SwiftData
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct CryptidCoordinatesApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
-        
         WindowGroup {
-            MapView()
+            ContentView()
         }
     }
 }
