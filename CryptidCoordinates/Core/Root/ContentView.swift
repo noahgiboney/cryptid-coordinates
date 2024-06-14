e1476ac096cbc49aefd55eeb3c72a40ea1d242ff
Noah Giboney <noahgiboney@gmail.com>
2024-06-13 20:24:28 -0700

    remove script

--- CryptidCoordinates/Core/Root/ContentView.swift
+++ CryptidCoordinates/Core/Root/ContentView.swift
@@ -11,40 +11,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Upload") {
-            uploadJSON()
-        }
-    }
-    
-    func uploadJSON() {
-        do {
-            let locations = Locations.allLocations
-            let db = Firestore.firestore()
-            
-            for item in locations {
-                do {
-                    let docRef = db.collection("locations").document(UUID().uuidString)
-                    if let enc = try? Firestore.Encoder().encode(item) {
-                        try docRef.setData(enc)
-                        print("Document \(item.id) successfully written!")
-                    } else {
-                        print("Error encoding item at index \(index): \(item)")
-                    }
-                } catch {
-                    print("Error writing document at index \(index) for item \(item.id): \(error)")
-                }
-            }
-            
-            print("Data successfully uploaded to Firestore!")
-        } catch {
-            print("Error reading or uploading JSON data: \(error)")
-        }
-    }
-}
-
-extension String {
-    func replacingSpacesWithHyphens() -> String {
-        return self.replacingOccurrences(of: " ", with: "-")
+        TabBarView()
     }
 }
 
