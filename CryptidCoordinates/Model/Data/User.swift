bff0a11255e9c6de6e8d8794a09e1f2a3fcbe019
Noah Giboney <noahgiboney@gmail.com>
2024-07-08 11:52:56 -0700

    user location to user model

--- CryptidCoordinates/Model/Data/User.swift
+++ CryptidCoordinates/Model/Data/User.swift
@@ -5,17 +5,32 @@
 //  Created by Noah Giboney on 6/15/24.
 //
 
+import MapKit
 import Foundation
 
 struct User: Codable, Identifiable, Equatable {
     var id: String
     var name: String
     var profilePicture = ProfilePicture.person
+    var score = 0
+    
+    var latitude: Double?
+    var longitude: Double?
     
     static func ==(lhs: User, rhs: User) -> Bool {
         return lhs.id == rhs.id &&
         lhs.name == rhs.name &&
         lhs.profilePicture == rhs.profilePicture
+    }
+}
+
+// MARK: Util
+extension User {
+    var location: CLLocationCoordinate2D? {
+        if let latitude, let longitude{
+            return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        }
+        return nil
     }
 }
 
