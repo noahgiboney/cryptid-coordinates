9dc1d9d0b3d0d6a670f4bfce495a89d132c89d78
Noah Giboney <noahgiboney@gmail.com>
2024-07-08 11:53:50 -0700

    back button component

--- CryptidCoordinates/View/Components/BackButton.swift
+++ CryptidCoordinates/View/Components/BackButton.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct BackButton: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "chevron.left")
+        }
     }
 }
 
