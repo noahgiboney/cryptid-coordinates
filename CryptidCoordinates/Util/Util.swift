a4c23121e8850a46e1909d007916981ec006cb22
Noah Giboney <noahgiboney@gmail.com>
2024-07-14 16:33:44 -0700

    function to compute distance betweent two points

--- CryptidCoordinates/Util/Util.swift
+++ CryptidCoordinates/Util/Util.swift
@@ -5,6 +5,7 @@
 //  Created by Noah Giboney on 7/7/24.
 //
 
+import CoreLocation
 import UIKit
 
 extension UINavigationController: UIGestureRecognizerDelegate {
@@ -15,5 +16,12 @@ extension UINavigationController: UIGestureRecognizerDelegate {
     
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         return viewControllers.count > 1
+    }
+}
+
+extension CLLocationCoordinate2D {
+    func distance(from location: CLLocation) -> CLLocationDistance {
+        let selfLocation = CLLocation(latitude: self.latitude, longitude: self.longitude)
+        return selfLocation.distance(from: location)
     }
 }
