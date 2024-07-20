adbca53e0302ab726225d41bbf36bb535f55001d
Noah Giboney <noahgiboney@gmail.com>
2024-07-19 21:23:58 -0700

    new loading view

---
+++ CryptidCoordinates/View/Components/LocationScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationScrollView.swift
+//  CryptidCoordinates
+//
+//  Created by Noah Giboney on 7/19/24.
+//
+
+import SwiftUI
+
+struct LocationScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationScrollView()
+}
--- CryptidCoordinates/View/Home/NearView.swift
+++ CryptidCoordinates/View/Home/NearView.swift
@@ -1,5 +1,5 @@
 //
-//  NearbyView.swift
+//  NearView.swift
 //  CryptidCoordinates
 //
 //  Created by Noah Giboney on 7/6/24.
@@ -10,28 +10,36 @@ import SwiftUI
 
 struct NearView: View {
     @Environment(LocationStore.self) var store
-    @StateObject var locationManager = LocationManager()
+    @EnvironmentObject var locationManager: LocationManager
     
     var body: some View {
         NavigationStack {
             Group {
                 if locationManager.lastKnownLocation == nil {
-                    locationOffView
+                    LocationUnavailableView(message: "Share your location to experience the haunt near you")
                 } else {
                     scrollView
                 }
             }
             .navigationTitle("Near You")
-            .onAppear {
-                locationManager.start()
-            }
         }
     }
     
     var scrollView: some View {
         ScrollView {
-            ForEach(store.nearLocations) {
-                LocationPreviewView(location: $0)
+            VStack(spacing: 45){
+                if store.didLoadNearLocations {
+                    ForEach(store.nearLocations) { location in
+                        NavigationLink {
+                            LocationView(location: location)
+                        } label: {
+                            LocationPreviewView(location: location, size: .preview)
+                        }
+                    }
+                } else {
+                    LoadingView()
+                        .offset(y: 100)
+                }
             }
         }
         .task {
@@ -40,48 +48,11 @@ struct NearView: View {
             }
         }
     }
-    
-//    var listView: some View {
-//        List {
-//            ForEach(exploreModel.locations) { location in
-//                LocationPreviewView(location: location)
-//                    .background {
-//                        NavigationLink("") {
-//                            LocationView(location: location)
-//                                .onAppear { isShowingMenu = false }
-//                                .onDisappear { isShowingMenu = true }
-//                        }
-//                        .opacity(0.0)
-//                    }
-//                    .listRowSeparator(.hidden)
-//                    .listRowInsets(EdgeInsets())
-//                    .task {
-//                        if location == exploreModel.locations.last {
-//                            try? await exploreModel.populateLocations()
-//                        }
-//                    }
-//            }
-//        }
-//        .listStyle(.plain)
-//        .listRowSpacing(30)
-//    }
-//    
-    var locationOffView: some View {
-        VStack {
-            ContentUnavailableView("Share your location to experience the haunt nearby", systemImage: "location.slash")
-                .overlay {
-                    LocationButton {
-                        locationManager.start()
-                    }
-                    .clipShape(Capsule())
-                    .offset(y: 100)
-                }
-        }
-    }
 }
 
 #Preview {
     NearView()
         .environment(LocationStore())
         .environmentObject(LocationManager())
+        .preferredColorScheme(.dark)
 }
