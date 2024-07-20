bef94eb6c8b28b4524c480a2a0ffcfff69e945bb
Noah Giboney <noahgiboney@gmail.com>
2024-07-19 21:33:33 -0700

    updated location unavailable view

--- CryptidCoordinates/View/Components/LocationUnavailableView.swift
+++ CryptidCoordinates/View/Components/LocationUnavailableView.swift
@@ -12,16 +12,18 @@ struct LocationUnavailableView: View {
     @EnvironmentObject var locationManager: LocationManager
     
     var body: some View {
-        ContentUnavailableView(message, systemImage: "location")
-            .overlay {
-                Button {
-                    locationManager.start()
-                } label: {
-                    Label("Current Location", systemImage: "location.fill")
-                }
-                .buttonStyle(.bordered)
-                .offset(y: 100)
+        ContentUnavailableView(label: {
+            Label("Location Unavailable", systemImage: "location")
+        }, description: {
+            Text(message)
+        }, actions: {
+            Button {
+                locationManager.start()
+            } label: {
+                Label("Current Location", systemImage: "location.fill")
             }
+            .buttonStyle(.bordered)
+        })
     }
 }
 
