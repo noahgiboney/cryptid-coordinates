8e812528bdc373f8464c20ffa51a464c29fa9aa3
Noah Giboney <noahgiboney@gmail.com>
2024-07-30 15:54:30 -0700

    feat: map marker images loading with progress view

--- CryptidCoordinates/View/Map/LocationMarkerView.swift
+++ CryptidCoordinates/View/Map/LocationMarkerView.swift
@@ -10,31 +10,65 @@ import SwiftUI
 
 struct LocationMarkerView: View {
     var url: URL?
+    @State private var loadState: LoadState = .loading
+    @State private var image: UIImage?
+    
+    var outlineColor: Color {
+        switch loadState {
+        case .loading:
+            return .white
+        case .loaded:
+            return .white
+        case .error:
+            return .gray.opacity(0.5)
+        }
+    }
     
     var body: some View {
-        KFImage(url)
-            .placeholder { _ in
-                ZStack {
-                    Circle().fill(.white)
-                        .frame(width: 50, height: 50)
-                    
-                    Image(systemName: "camera")
-                }
+        ZStack {
+            Circle().fill(.white)
+                .frame(width: 50, height: 50)
+            
+            switch loadState {
+            case .loading:
+                ProgressView()
+            case .loaded:
+                Image(uiImage: image!)
+                    .resizable()
+                    .clipShape(Circle())
+                    .shadow(radius: 10)
+                    .frame(maxWidth: 50, maxHeight: 50)
+            case .error:
+                Image(systemName: "camera")
+                    .foregroundStyle(.black)
             }
-            .loadDiskFileSynchronously()
-            .cacheMemoryOnly()
-            .fade(duration: 0.25)
-            .resizable()
-            .frame(maxWidth: 50, maxHeight: 50)
-            .clipShape(Circle())
-            .overlay(
-                Circle().stroke(Color.white, lineWidth: 2.5)
-            )
-            .shadow(radius: 10)
+        }
+        .overlay(
+            Circle().stroke(outlineColor, lineWidth: 2.5)
+        )
+        .onAppear { downloadImage() }
+    }
+    
+    func downloadImage() {
+        guard loadState == .loading else { return }
+        
+        guard let url = url else { return }
+        let resource = ImageResource(downloadURL: url)
+
+        KingfisherManager.shared.retrieveImage(with: resource, options: [.cacheOriginalImage], progressBlock: nil) { result in
+            switch result {
+            case .success(let value):
+                DispatchQueue.main.async {
+                    image = value.image
+                    loadState = .loaded
+                }
+            case .failure(_):
+                loadState = .error
+            }
+        }
     }
 }
 
 #Preview {
     LocationMarkerView()
-        .preferredColorScheme(.dark)
 }
