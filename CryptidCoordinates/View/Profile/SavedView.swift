6be4e8c8dcb5d4fe54271527451f42612f3dc679
Noah Giboney <noahgiboney@gmail.com>
2024-07-31 18:57:38 -0700

    feat: remove search

--- CryptidCoordinates/View/Profile/SavedView.swift
+++ CryptidCoordinates/View/Profile/SavedView.swift
@@ -11,22 +11,20 @@ import SwiftUI
 
 struct SavedView: View {
     @Environment(Saved.self) var saved
-    @State private var searchText = ""
     
     var body: some View {
         Group {
             if saved.locations.isEmpty {
                 ContentUnavailableView("No Locations Saved", systemImage: "house.lodge")
             } else{
-                SavedListView(locations: saved.locations, searchText: searchText)
+                SavedListView(locations: saved.locations)
             }
         }
         .navigationTitle("Saved")
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden()
-        .searchable(text: $searchText)
         .toolbar {
-            ToolbarItem(placement: .topBarLeading){
+            ToolbarItem(placement: .topBarLeading) {
                 BackButton()
             }
         }
@@ -35,41 +33,25 @@ struct SavedView: View {
 
 struct SavedListView: View {
     var locations: Set<String>
-    var searchText: String
     @Environment(Saved.self) var saved
     @Query var savedLocations: [Location]
     
-    init(locations: Set<String>, searchText: String) {
+    init(locations: Set<String>) {
         self.locations = locations
-        self.searchText = searchText
-        
-        var predicate: Predicate<Location>
-        
-        if searchText.isEmpty {
-            predicate = #Predicate { locations.contains($0.id) }
-        } else {
-            predicate = #Predicate { location in
-                return locations.contains(location.id) &&
-                location.name.localizedStandardContains(searchText)
-            }
-        }
-        
-        self._savedLocations = .init(filter: predicate, sort: [SortDescriptor(\Location.name)], animation: .default)
+        self._savedLocations = .init(filter: #Predicate<Location> { locations.contains($0.id) }, sort: [SortDescriptor(\Location.name)], animation: .default)
     }
     
     var body: some View {
-        List {
-            ForEach(savedLocations) { location in
-                LocationRowView(location: location)
-                    .swipeActions {
-                        Button {
-                            saved.update(location)
-                        } label: {
-                            Image(systemName: "bookmark.slash.fill")
-                        }
-                        .tint(Color("AccentColor"))
+        List(savedLocations) { location in
+            LocationRowView(location: location)
+                .swipeActions {
+                    Button {
+                        saved.update(location)
+                    } label: {
+                        Image(systemName: "bookmark.slash.fill")
                     }
-            }
+                    .tint(Color("AccentColor"))
+                }
         }
     }
 }
