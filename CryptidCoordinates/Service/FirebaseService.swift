25f19fc033fde335bc4051a7c6f91088a0e13345
Noah Giboney <noahgiboney@gmail.com>
2024-09-02 20:59:11 -0700

    feat: generic firebase service

---
+++ CryptidCoordinates/Service/FirebaseService.swift
@@ -0,0 +1,35 @@
+//
+//  FirebaseService.swift
+//  CryptidCoordinates
+//
+//  Created by Noah Giboney on 9/2/24.
+//
+
+import Firebase
+import Foundation
+
+struct FirebaseService {
+    
+    static let shared = FirebaseService()
+    
+    func fetchOne<T: DataModel>(id: String, ref: CollectionReference) async throws -> T? {
+        let snapshot = try await ref.document(id).getDocument()
+        return try snapshot.data(as: T.self)
+    }
+    
+    func fetchData<T: DataModel>(ref: CollectionReference, id: String? = nil) async throws -> [T] {
+        let snapshot = try await ref.getDocuments()
+        return snapshot.documents.compactMap { try? $0.data(as: T.self) }
+    }
+    
+    func setData<T:DataModel>(object: T, ref: CollectionReference) async throws {
+        guard let id = object.id as? String else { return }
+        
+        let data = try Firestore.Encoder().encode(object)
+        try await ref.document(id).setData(data)
+    }
+    
+    func delete(id: String, ref: CollectionReference) {
+        ref.document(id).delete()
+    }
+}
---
+++ CryptidCoordinates/Util/Index.swift
@@ -0,0 +1,10 @@
+//
+//  Index.swift
+//  CryptidCoordinates
+//
+//  Created by Noah Giboney on 9/2/24.
+//
+
+import Foundation
+
+typealias DataModel = Codable & Identifiable & Hashable
