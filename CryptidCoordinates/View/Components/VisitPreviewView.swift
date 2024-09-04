8bb675d9ddca494987403912545813081d8c0d9e
Noah Giboney <noahgiboney@gmail.com>
2024-09-04 14:49:01 -0700

    fix: visit preview loading ui

--- CryptidCoordinates/View/Components/VisitPreviewView.swift
+++ CryptidCoordinates/View/Components/VisitPreviewView.swift
@@ -11,84 +11,23 @@ import SwiftData
 import SwiftUI
 
 struct VisitPreviewView: View {
-    var location: Location
-    var visitDate: Timestamp
+    let location: Location
+    let visitDate: Timestamp
+    @Environment(\.colorScheme) var colorScheme
     @State private var loadState: LoadState = .loading
     @State private var image: UIImage?
     
-    var averageColor: UIColor {
+    private var averageColor: UIColor {
         image?.findAverageColor(algorithm: .simple) ?? .gray
     }
     
-    var body: some View {
-        Group {
-            switch loadState {
-            case .loading:
-                ProgressView()
-            case .loaded:
-                previewView
-            case .error:
-                Image(systemName: "camera")
-            }
-        }
-        .onAppear {
-            downloadImage()
-        }
-    }
-    
-    var previewView: some View {
-        VStack(alignment: .center) {
-            NavigationLink {
-                LocationView(location: location)
-            } label: {
-                if let uiImage = image {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 75, height: 75)
-                        .clipShape(Circle())
-                        .overlay(
-                            Circle().stroke(Color(uiColor: averageColor), lineWidth: 3)
-                        )
-                        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 0)
-                } else {
-                    ZStack {
-                        Circle()
-                            .fill(Color(uiColor: .systemBackground))
-                            .frame(width: 75, height: 75)
-                        
-                        Image(systemName: "camera")
-                            .foregroundStyle(.primary)
-                    }
-                    .overlay(
-                        Circle().stroke(Color(uiColor: averageColor), lineWidth: 3)
-                    )
-                }
-            }
-            
-            VStack(alignment: .center) {
-                Text(location.name)
-                    .lineLimit(1)
-                    .truncationMode(.tail)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.primary)
-                
-                Text("Visited \(visitDate.dateValue().formatted(date: .abbreviated, time: .shortened))")
-                    .font(.caption2)
-                    .foregroundStyle(.gray)
-            }
-        }
-        .padding(.vertical)
-        .containerRelativeFrame(.horizontal, count: 2, spacing: 10)
-    }
-    
-    func downloadImage() {
+    private func downloadImage() {
         guard loadState == .loading else { return }
-        
         guard let url = URL(string: location.imageUrl) else { return }
-        let resource = ImageResource(downloadURL: url)
         
-        KingfisherManager.shared.retrieveImage(with: resource, options: [.cacheOriginalImage], progressBlock: nil) { result in
+        let resource = KF.ImageResource(downloadURL: url)
+        
+        KingfisherManager.shared.retrieveImage(with: resource) { result in
             switch result {
             case .success(let value):
                 DispatchQueue.main.async {
@@ -96,8 +35,82 @@ struct VisitPreviewView: View {
                     loadState = .loaded
                 }
             case .failure(_):
-                loadState = .loaded
+                DispatchQueue.main.async {
+                    loadState = .loaded
+                }
             }
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            NavigationLink {
+                LocationView(location: location)
+            } label: {
+                locationNavLinkLabel
+            }
+            
+            visitInfoView
+        }
+        .containerRelativeFrame(.horizontal, count: 2, spacing: 10)
+        .padding(.vertical)
+        .onAppear {
+            downloadImage()
+        }
+    }
+    
+    private var visitInfoView: some View {
+        VStack(alignment: .center) {
+            Text(location.name)
+                .lineLimit(1)
+                .truncationMode(.tail)
+                .fontWeight(.semibold)
+                .foregroundStyle(.primary)
+            
+            Text("Visited \(visitDate.dateValue().formatted(date: .abbreviated, time: .shortened))")
+                .font(.caption2)
+                .foregroundStyle(.gray)
+        }
+    }
+    
+    @ViewBuilder
+    private var locationNavLinkLabel: some View {
+        if loadState == .loaded {
+            if let uiImage = image {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 75, height: 75)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color(uiColor: averageColor), lineWidth: 3)
+                    )
+                    .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 0)
+            } else {
+                ZStack {
+                    Circle()
+                        .fill(Color(uiColor: .systemBackground))
+                        .frame(width: 75, height: 75)
+                    
+                    Image(systemName: "eye.slash")
+                        .foregroundStyle(colorScheme == .dark ? .white : .black)
+                        .imageScale(.large)
+                }
+                .overlay(
+                    Circle().stroke(Color(uiColor: averageColor), lineWidth: 3)
+                )
+            }
+        } else {
+            ZStack {
+                Circle()
+                    .fill(Color(uiColor: .systemBackground))
+                    .frame(width: 75, height: 75)
+                
+                ProgressView()
+            }
+            .overlay(
+                Circle().stroke(Color(uiColor: averageColor), lineWidth: 3)
+            )
         }
     }
 }
